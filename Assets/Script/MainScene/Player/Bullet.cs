adf6a8671526a1fd2b5cc6b798f07599e5875a66
seiyaohtake <seiyaohtake@gmail.com>
2016-01-30 03:20:12 +0900

    fist commit

---
+++ Assets/Script/MainScene/Player/Bullet.cs
@@ -0,0 +1,20 @@
+﻿/*更新日時2016/01/30 3:16
+ * Q.なにこれ
+ * A.弾のベースプログラムです
+ * 
+ */
+using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/MainScene/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿/*更新日時2016/01/30　3:14
+ * Q.なにこれ
+ * A.プレイヤー（敵と自機）のベースのスクリプトです
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
