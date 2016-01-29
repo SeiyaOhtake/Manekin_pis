7e03fe06833ee649f9ff9e9539e8d0b871fc7eb0
seiyaohtake <seiyaohtake@gmail.com>
2016-01-30 08:49:29 +0900

    testBullet

---
+++ Assets/Ohtake/Script/Move_Bullet_test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Bullet_test : MonoBehaviour {
+    public float flap = 5f;//ジャンプする強さ
+    //public Rigidbody2D rb;
+
+    public int speed = 10;//弾のスピード
+
+    //private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        //rb = gameObject.GetComponent<Rigidbody2D>();
+        //startTime = Time.time;
+
+        //弾発射
+        GetComponent<Rigidbody2D>().velocity = transform.right.normalized * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (startTime - Time.time % 5 == 0)
+        //{
+            // (0,1)方向に瞬間的に力を加えて跳ねさせる
+            //rb.AddForce(Vector2.left * flap);
+        //}
+
+	}
+}
---
+++ Assets/Ohtake/Script/Move_Test_MP.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Test_MP : MonoBehaviour {
+    public float flap = 1f;//ジャンプする強さ
+    public bool JumpCheck = false;//ジャンプ中か否か
+    public Rigidbody2D rb;
+
+    // PlayerBulletプレハブ
+    public GameObject bullet;
+
+    // Startメソッドをコルーチンとして呼び出す
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            // 0.05秒待つ
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Destroy(bullet);
+            // 0.05秒待つ
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+	// Use this for initialization
+	//void Start () {
+    //    rb = gameObject.GetComponent<Rigidbody2D>();
+    //}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+
+
+        // スペースキーが押されたらジャンプ
+        if (Input.GetKeyDown("space"))
+        {
+            if (!JumpCheck)//ジャンプ中じゃなかったら
+            {
+                // 落下速度をリセット
+                rb.velocity = Vector2.zero;
+                // (0,1)方向に瞬間的に力を加えて跳ねさせる
+                rb.AddForce(Vector2.up * flap);
+                JumpCheck = true;//ジャンプしてるに
+            }
+        }
+
+        DestroyBullet();
+	}
+}
