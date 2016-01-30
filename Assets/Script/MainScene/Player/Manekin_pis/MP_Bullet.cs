bdd2529cf714abb8e035c1175cf9ded6f8970702
seiyaohtake <seiyaohtake@gmail.com>
2016-01-30 13:09:52 +0900

    弾が出るようになった

--- Assets/Script/MainScene/Player/Manekin_pis/MP_Bullet.cs
+++ Assets/Script/MainScene/Player/Manekin_pis/MP_Bullet.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class MP_Bullet : MonoBehaviour {
+   
+    public int speed = 10;//弾のスピード
 
+    public GameObject bullet;
+
+    IEnumerator DestroyBullet()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            // 1秒後消す
+            yield return new WaitForSeconds(1.0f);
+            Destroy(bullet);
+
+        }
+    }
 	// Use this for initialization
 	void Start () {
-	
+        //弾発射
+        GetComponent<Rigidbody2D>().velocity = transform.right.normalized * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Stage")
+        {
+            StartCoroutine("DestroyBullet");
+        }
+    }
 }
--- Assets/Script/MainScene/Player/Manekin_pis/Manekin_Pis.cs
+++ Assets/Script/MainScene/Player/Manekin_pis/Manekin_Pis.cs
@@ -3,10 +3,20 @@ using System.Collections;
 
 public class Manekin_Pis : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // PlayerBulletプレハブ
+    public GameObject bullet;
+
+    // Startメソッドをコルーチンとして呼び出す
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            // 0.05秒間隔で撃つ
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
