3bd596247e1f0999c6756f0c820b837e90a2c661
seiyaohtake <seiyaohtake@gmail.com>
2016-01-31 10:50:01 +0900

    Add AudioScript

---
+++ Assets/Script/Audio/AudioScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+    public AudioSource[] sources;
+	// Use this for initialization
+	void Start () {
+        sources = gameObject.GetComponents<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ジャンプ音
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PlaySE(0);
+        }
+	}
+
+    public void PlaySE(int i)
+    {
+        sources[i].Play();
+    }
+}
